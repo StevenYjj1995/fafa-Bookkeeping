5b7f36f7d210cee9f16bf1776d886e2580c06065
steven <steven.yjj1995@gmail.com>
2021-04-09 20:31:53 +0800

    创建Icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,22 +1,39 @@
 <template>
-<div class="nav">
-  <router-link to="/money">记账</router-link>
-  |
-  <router-link to="/labels">标签</router-link>
-  |
-  <router-link to="/statistics">统计</router-link>
-</div>
+  <div class="nav">
+    <router-link to="/money">
+      <Icon name="money"/>
+      记账
+    </router-link>
+    |
+    <router-link to="/labels">
+      <Icon name="labels"/>
+      标签
+    </router-link>
+    |
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
+  </div>
 </template>
 
 <script lang="ts">
-export default {
-name: "Nav"
+
+// eslint-disable-next-line no-undef
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/));
+} catch (err) {
+  console.log(err);
 }
+export default {
+  name: 'Nav',
+};
 </script>
 
 <style lang="scss" scoped>
 
-.nav{
+.nav {
   border: 1px solid red;
 }
 </style>
