f9ba0bad3c4d0bd5e6a200f72199f7a2b96ef20b
steven <steven.yjj1995@gmail.com>
2021-04-13 23:33:41 +0800

    成功使添加标签按钮按取消时不显示提示框，并添加条件标题名不能含有空格

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -4,8 +4,18 @@ import Component from 'vue-class-component';
 export class TagHelper extends Vue{
   createTag():void{
     const name =window.prompt('请输入标签名')
-    if(!name){return window.alert('标签名不能为空')}
-    this.$store.commit('createTag',name)
+    if(name){
+      if(name.indexOf(' ')>=0){
+          return window.alert('标签名不能含有空格')
+      }
+      this.$store.commit('createTag',name)
+    }else if(name===''){
+      return window.alert('标签名不能为空')
+    }
+    else{
+      return
+    }
+
   }
 }
 export default TagHelper
