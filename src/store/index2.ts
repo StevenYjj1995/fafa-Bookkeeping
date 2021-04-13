b00cd7209491ce44edf377d5fc29bb86d2fa8e52
steven <steven.yjj1995@gmail.com>
2021-04-13 17:13:59 +0800

    模块化的store，删除了models

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,32 +1,9 @@
-import {recordListModel} from '@/models/recordListModel';
-import {tagListModel} from '@/models/tagListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
-//record store
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => recordListModel.create(record),
-  //1全局变量太多
-  //2严重依赖window
-  //tag store
-  tagList: tagListModel.fetch(),
-  createTag: (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === 'duplicated') {
-      window.alert('标签名重复，请重新输入');
-    } else if (message === 'success') {
-      window.alert('创建标签成功');
-    }
-
-  },
-  removeTag: (id: string) => {
-    return tagListModel.remove(id);
-  },
-  updateTag: (id: string, name: string) => {  //object:Exclude<Tag,'id'>
-    return tagListModel.update(id, name);
-  },
-  findTag (id: string) {
-    return this.tagList.filter(t => t.id === id)[0];
-  }
+  ...recordStore,
+  ...tagStore
 };
-
-export default store
+console.log(store);
+export default store;
---
+++ src/store/recordStore.ts
@@ -0,0 +1,23 @@
+import clone from '@/lib/clone';
+
+const localStorageKeyName = 'recordList';
+const recordStore = {
+  recordList: [] as RecordItem[],
+  fetchRecords(): RecordItem[] {
+    this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.recordList;
+  },
+  saveRecords(): void {
+    window.localStorage.setItem(localStorageKeyName,
+      JSON.stringify(this.recordList));
+  },
+  createRecord(record: RecordItem): void {
+    const record2: RecordItem = clone(record);
+    record2.createdAt = new Date();
+    this.recordList && this.recordList.push(record2);
+    //this.recordList?.push(record2);可选链语法
+    recordStore.saveRecords();
+  }
+};
+recordStore.fetchRecords();
+export default recordStore;
