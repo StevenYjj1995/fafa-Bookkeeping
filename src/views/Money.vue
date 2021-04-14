426adcbf5ba6231f8d7d80f64e008f643ee0ebe5
steven <steven.yjj1995@gmail.com>
2021-04-14 14:23:44 +0800

    删除Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -12,7 +12,6 @@
 <script lang="ts">
 import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
@@ -24,7 +23,7 @@ import recordTypeList from '@/constants/recordTypeList';
 // const recordList:Record[] = model.fetch()
 window.localStorage.setItem('version', '0.0.1 ');
 @Component({
-  components: {Tabs, Button, FormItem, Tags, Notes: FormItem, Types, NumberPad},
+  components: {Tabs, Button, FormItem, Tags, Notes: FormItem, NumberPad},
 })
 export default class Money extends Vue {
   recordTypeList = recordTypeList
