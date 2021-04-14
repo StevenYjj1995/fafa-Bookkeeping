1c143b833a2ae7a946c0079acdc3660bfba21576
steven <steven.yjj1995@gmail.com>
2021-04-14 16:12:19 +0800

    完成列表的css

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,11 +2,18 @@
   <Layout>
     <Tabs :data-source="recordTypeList" :value.sync="type" class-prefix="type"/>
     <Tabs :data-source="intervalList" :value.sync="interval" class-prefix="interval"/>
-    <div>
-      type:{{ type }}
-      <br>
-      interval:{{ interval }}
-    </div>
+    <ol>
+      <li v-for="(group,index) in result" :key="index">
+        <h3 class="title">{{group.title}}</h3>
+        <ol  class="record">
+          <li v-for="item in group.items" :key="item.id">
+            <span>{{tagString(item.tags)}}</span>
+            <span class="itemNotes">{{item.notes}}</span>
+            <span>￥{{ item.amount }}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
 
@@ -21,6 +28,33 @@ import recordTypeList from '@/constants/recordTypeList';
   components: {Tabs},
 })
 export default class Statistics extends Vue {
+  // eslint-disable-next-line no-undef
+  tagString(tags:Tag[]){
+    return tags.length ===0 ? '无' : tags.join(',')
+  }
+  get recordList() {
+    // eslint-disable-next-line no-undef
+    return (this.$store.state as RootState).recordList;
+  }
+
+  get result() {
+    const {recordList} = this;
+    // eslint-disable-next-line no-undef
+    type HashTableValue = {title:string,items:RecordList[]}
+    // eslint-disable-next-line no-undef
+    const hashTable: { [key: string]: HashTableValue } = {};
+    for (let i = 0; i < recordList.length; i++) {
+      const [date, time] = recordList[i].createdAt!.split('T');
+      hashTable[date] = hashTable[date] || {title:date,items:[]};
+      hashTable[date].items.push(recordList[i])
+    }
+    return hashTable;
+  }
+
+  beforeCreate() {
+    this.$store.commit('fetchRecords');
+  }
+
   type = '-';
   interval = 'day';
   intervalList = intervalList;
@@ -30,18 +64,45 @@ export default class Statistics extends Vue {
 </script>
 
 <style lang="scss" scoped>
-::v-deep .type-tabs-item {
-  background: white;
-
-  &.selected {
-    background: #c4c4c4;
-
-    &::after {
-      display: none;
-    }
+%item{
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  align-content: center;
+}
+.title{
+  @extend %item
+}
+.record{
+  > li{
+    background: white;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+    @extend %item
+  }
+  > li:last-child{
+    border-bottom: none;
   }
 }
-::v-deep .interval-tabs-item{
-  height: 48px;
+.itemNotes{
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999;
+}
+::v-deep {
+  .type-tabs-item {
+    background: white;
+
+    &.selected {
+      background: #c4c4c4;
+
+      &::after {
+        display: none;
+      }
+    }
+  }
+  .interval-tabs-item {
+    height: 48px;
+  }
 }
 </style>
